fcb00ff867ffc2e2d614733780d20dadc7f0c691
Micha Rybiski <michalrybinski1@gmail.com>
2020-08-31 22:12:34 +0200

    initial commit

---
+++ src/takeLatestBy.js
@@ -0,0 +1,25 @@
+import { fork, cancel, takeEvery } from 'redux-saga/effects'
+
+export default function * takeLatestBy (
+  patternOrChannel,
+  worker,
+  getKey,
+  ...args
+) {
+  const tasks = {}
+
+  yield takeEvery(
+    patternOrChannel,
+    function * () {
+      const action = arguments[arguments.length - 1]
+      const key = getKey(action)
+      const previousTask = tasks[key]
+      if (previousTask) {
+        yield cancel(previousTask)
+      }
+
+      tasks[key] = yield fork(worker, ...arguments)
+    },
+    ...args
+  )
+}
