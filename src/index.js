e7544dbec531cc9117c32eb9510602fb3c5f4dfb
Micha Rybiski <michalrybinski1@gmail.com>
2020-09-01 14:39:35 +0200

    use context instead of local variable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import { fork, cancel, takeEvery } from 'redux-saga/effects'
+import {
+  fork,
+  cancel,
+  takeEvery,
+  getContext,
+  setContext
+} from 'redux-saga/effects'
+
+function * internalWorker (worker, getKey, ...args) {
+  const tasks = yield getContext('takeLatestByTasks')
+  const key = getKey(...args)
+  const previousTask = tasks[key]
+  if (previousTask) {
+    yield cancel(previousTask)
+  }
+
+  tasks[key] = yield fork(worker, ...args)
+}
 
 function * takeLatestBy (patternOrChannel, worker, getKey, ...args) {
-  const tasks = {}
-
-  yield takeEvery(
-    patternOrChannel,
-    function * () {
-      const key = getKey(...arguments)
-      const previousTask = tasks[key]
-      if (previousTask) {
-        yield cancel(previousTask)
-      }
-
-      tasks[key] = yield fork(worker, ...arguments)
-    },
-    ...args
-  )
+  const takeLatestByTasks = yield getContext('takeLatestByTasks')
+  if (!takeLatestByTasks) yield setContext({ takeLatestByTasks: {} })
+  yield takeEvery(patternOrChannel, internalWorker, worker, getKey, ...args)
 }
 
 module.exports = takeLatestBy
